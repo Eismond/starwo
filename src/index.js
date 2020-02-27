aeccb3a814c27f0a8fce89bc735435811b6d3070
Eismond <a.eismond@gmail.com>
2020-02-28 00:18:24 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+// https://swapi.co/
+
+/*fetch('https://swapi.co/api/people/1')// get promise
+  .then((res) => {
+    //console.log(res);
+    return res.json();
+  })
+  .then((body) => {
+    console.table(body);
+  })*/
+
+
+
+/*const getResource = async (url) => {
+  console.log('starwo start')
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch` + `${res.status}`)
+  }
+  console.log(res);
+  const body = await res.json();
+  return body;
+};
+
+getResource('https://swapi.co/api/people/1цмц/')
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.log(err);
+  });*/
