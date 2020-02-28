14413253dbe950d325e14fad0eff68f3686a606e
Eismond <a.eismond@gmail.com>
2020-02-28 23:16:44 +0200

    start adding component

---
+++ src/components/app/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import PlanetDetails from '../planet-details';
+import StarshipDetails from '../starship-details';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+};
